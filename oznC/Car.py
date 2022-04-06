462fba5a20926cac8c49a972b6b9d9917d3c5863
Yudjerick <denispetrenko2004@gmail.com>
2022-04-06 14:02:25 +0300

    add C

---
+++ oznC/Car.py
@@ -0,0 +1,5 @@
+class Car:
+    def __init__(self, mark, voltage, year):
+        self.mark = mark
+        self.voltage = voltage
+        self.year = year
---
+++ oznC/PassengerCar.py
@@ -0,0 +1,14 @@
+import Car
+
+class PassengerCar(Car):
+    def __init__(self, mark, voltage, year, passeners):
+        super().__init__(mark, voltage, year)
+        self.passengers = passengers
+        self.repair_book = {}
+        
+    def add_detail(self, detail, year):
+        self.repair_book[detail] = year
+
+    def get_changeyear(self, detail):
+        return self.repair_book[detail]
+    
